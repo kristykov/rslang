86c8f1da27b4233b985c8ad97771d4e15454ee80
AnastasiyaPoleshuk <anastasiyapaliashuk@gmail.com>
2022-02-10 15:28:38 +0300

    fix: bug in change log in and sing in page

--- src/app/views/authorizationView/authorizationView.ts
+++ src/app/views/authorizationView/authorizationView.ts
@@ -1,11 +1,23 @@
 import './authorizationView.scss';
 import Component from '../_templates/component';
-import Header from '../_templates/header/header';
+import { Header } from '../_templates/header/header';
 import Footer from '../_templates/footer/footer';
 import defaultRoutes from '../../models/router/defaultRoutes';
+import { state } from '../../models/api/state/state';
 
 class AuthorizationView extends Component {
-  frontBlockContent = ` <section class="authorization-box">
+  authorizationBlockContent = ` <section class="authorization-box">
+      <img src="/assets/authorization-img.png" alt="authorization img" class="authorization-img">
+      <div class="authorization">
+        <h2 class="authorization-title">Nice to see you again</h2>
+        <span class="inputs-name">Name: <input type="text" class="text-input inputs" id="name"></span> 
+        <span class="inputs-name">Password: <input type="password" class="password-input inputs" id="password"></span> 
+        <a href="#${defaultRoutes.frontpage.path}"><button class="register-btn">sign in</button></a>
+        <a id="sign-in-link" class="view">don't have an account yet? Register now!</a>
+      </div>
+    </section>`;
+
+  registrationBlockContent = ` <section class="authorization-box">
       <img src="/assets/authorization-img.png" alt="authorization img" class="authorization-img">
       <div class="authorization">
         <h2 class="authorization-title">Register and get access to additional features of the application</h2>
@@ -13,9 +25,9 @@ class AuthorizationView extends Component {
         <span class="inputs-name">Name: <input type="text" class="text-input inputs" id="name"></span> 
         <span class="inputs-name">Password: <input type="password" class="password-input inputs" id="password"></span> 
         <a href="#${defaultRoutes.frontpage.path}"><button class="register-btn">Register</button></a>
+        <a id="register-link" class="view">Do you already have an account?</a>
       </div>
     </section>`;
-
   frontBlockWrapper = new Component('div', ['container']);
 
   header: Header;
@@ -23,6 +35,8 @@ class AuthorizationView extends Component {
   frontBlock: Component;
 
   footer: Footer;
+
+  frontBlockContent: string;
 
   constructor(root: HTMLElement) {
     super('div', ['authorization-view'], root);
@@ -33,8 +47,28 @@ class AuthorizationView extends Component {
       this.container,
     );
     this.footer = new Footer(this.container);
+    this.frontBlockContent = ``;
+
+    this.chooseView();
+  }
+
+  chooseView() {
+    this.frontBlockContent = this.registrationBlockContent;
     this.frontBlock.container.append(this.frontBlockWrapper.container);
     this.frontBlockWrapper.container.innerHTML = this.frontBlockContent;
+
+    this.frontBlock.container.addEventListener('click', (event) => {
+      let target = event.target as HTMLElement;
+
+      if (target.id == 'register-link') {
+        this.frontBlockContent = this.authorizationBlockContent;
+      } else {
+        this.frontBlockContent = this.registrationBlockContent;
+      }
+
+      this.frontBlock.container.append(this.frontBlockWrapper.container);
+      this.frontBlockWrapper.container.innerHTML = this.frontBlockContent;
+    });
   }
 }
 
